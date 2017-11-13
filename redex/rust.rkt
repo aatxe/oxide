7dbdd67a246d19e2c466be2b38cf19550796ee7c
Aaron Weiss <awe@pdgn.co>
2017-11-13 10:59:37 +0100

    Patched rust0 with tuple types, and explicit type annotations on bindings.

--- redex/rust.rkt
+++ redex/rust.rkt
@@ -9,15 +9,15 @@
 
   ;; top-level statements
   (tls ::=
-       (struct sid [(lt ι) ... α ...] {(x t) ...})
-       (enum sid [(lt ι) ... α ...] {(vid t ...) ...})
-       (fn f [(lt ι) ... α ...] ((x t) ...) { st ... }))
+       (struct sid [(lft ι) ... α ...] {(x t) ...})
+       (enum sid [(lft ι) ... α ...] {(vid t ...) ...})
+       (fn f [(lft ι) ... α ...] ((x t) ...) { st ... }))
 
   ;; statements
   (st ::=
       ;; let bindings (immutable and mutable)
-      (let lv = rv)
-      (let mut lv = rv)
+      (let (lv t) = rv)
+      (let mut (lv t) = rv)
       ;; assignment (only for mutable bindings)
       (lv = rv)
 
@@ -43,7 +43,7 @@
       (tup rv ...)
 
       ;; function calls
-      (f [(lt ι) ... t ...] (rv ...))
+      (f [(lft ι) ... t ...] (rv ...))
       ;; blocks (sequence of statements, takes value of last statement)
       (block st ...)
       ;; pattern matching
@@ -73,7 +73,7 @@
   ;; type parameters
   (typaram ::=
            α
-           (lt ι))
+           (lft ι))
 
   ;; types
   (t ::=
@@ -81,6 +81,8 @@
      α
      ;; references (&'a t)
      (ref ι t)
+     ;; tuple types
+     (tup t ...)
 
      ;; struct and enum names
      sid
@@ -116,16 +118,16 @@
  ;; valid top-level statements
  #:m tls (struct Point [] { (x num)
                             (y num) })
- #:m tls (struct Ref [(lt α) T] { (inner (ref α T)) })
+ #:m tls (struct Ref [(lft α) T] { (inner (ref α T)) })
  #:m tls (enum Option [T] { (None)
                             (Some T) })
  #:m tls (enum WeirdOption [T] { (None)
                                  (Some T)
                                  (Double T T) })
- #:m tls (fn main [] () { (let (tup x y) = (tup 1 2)) })
+ #:m tls (fn main [] () { (let [(tup x y) (tup num num)] = (tup 1 2)) })
  #:m tls (fn sum_to [] ((x num)) { (if (x = 0)
                                        1
                                        (x + (sum_to [] ((x + -1))))) })
 
  ;; invalid top-level statements
- #:f #:m tls (fn main [] () { (let (tup 1 2) = (tup 3 4)) }))
+ #:f #:m tls (fn main [] () { (let [(tup 1 2) (tup num num)] = (tup 3 4)) }))
