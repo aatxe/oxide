4149e4f297bc917dbc6288bc7b8340021af9292a
Aaron Weiss <awe@pdgn.co>
2019-01-25 18:13:32 -0500

    fraction normalization and scrapped my boilerplate

--- ocaml/oxide.ml
+++ ocaml/oxide.ml
@@ -39,53 +39,72 @@ type var_env = (place * (frac * ty)) list [@@deriving show]
 
 let var_env_lookup (gamma : var_env) (pi : place) : (frac * ty) = List.assoc pi gamma
 
+let rec add_frac (f1 : frac) (f2 : frac) =
+  match (f1, f2) with
+  | (Num n1, Num n2) -> Num (n1 + n2)
+  | (Add (n1, n2), f2) -> add_frac (add_frac n1 n2) f2
+  | (f1, Add (n1, n2)) -> add_frac f1 (add_frac n1 n2)
+  | (Div (n1, d1), Div (n2, d2)) -> Div (add_frac n1 n2, mult_frac d1 d2)
+  | (Div (n1, d1), Num n) -> Div (add_frac n1 (mult_frac d1 (Num n)), d1)
+  | (Num n, Div (n1, d1)) -> Div (add_frac n1 (mult_frac d1 (Num n)), d1)
+and mult_frac (f1 : frac) (f2 : frac)  =
+  match (f1, f2)  with
+  | (Num n1, Num  n2) -> Num (n1 * n2)
+  | (Add (n1, n2), f2) -> mult_frac (add_frac n1 n2) f2
+  | (f1, Add (n1, n2)) -> mult_frac f1 (add_frac n1 n2)
+  | (Div (n1, d1), Div (n2, d2)) -> Div (mult_frac n1 d2, mult_frac d1 n2)
+  | (Div (n1, d1), Num n) -> Div (mult_frac n1 (Num n), d1)
+  | (Num n, Div (n1, d1)) -> Div (mult_frac n1 (Num n), d1)
+
+let rec gcd a b =
+  match (a mod b) with
+  | 0 -> b
+  | r -> gcd b r
+
+let rec normalize (frac : frac) : frac =
+  match frac with
+  | Num x -> Num x
+  | Add (f1, f2) -> normalize (add_frac f1 f2)
+  | Div (Num n, Num d) ->
+      let gcd = gcd n d
+      in Div (Num (n / gcd), Num (d / gcd))
+  | Div (f1, f2) -> normalize (mult_frac f1 (Div (Num 1, f2)))
+
 let make_loan (gamma : var_env) (mu : muta) (from_pi : place) : var_env =
   let (curr_frac, tau) = var_env_lookup gamma from_pi
   in let base_gamma = List.remove_assoc from_pi gamma
   in match mu with
-     | Imm -> List.cons (from_pi, (Div (curr_frac, Num 2), tau)) base_gamma
+     | Imm -> List.cons (from_pi, (normalize (Div (curr_frac, Num 2)), tau)) base_gamma
      | Mut -> List.cons (from_pi, (Num 0, tau)) base_gamma
 
 let return_loan (gamma : var_env) (frac : frac) (from_pi : place) : var_env =
   let (curr_frac, tau) = var_env_lookup gamma from_pi
   in let base_gamma = List.remove_assoc from_pi gamma
-  in List.cons (from_pi, (Add (curr_frac, frac), tau)) base_gamma
+  in List.cons (from_pi, (normalize (Add (curr_frac, frac)), tau)) base_gamma
 
 let frac_to_muta (frac : frac) : muta =
   match frac with
   | Num 1 -> Mut
   | _ -> Imm
 
-let normalize (frac : frac) : frac = ??
+let rec walk (change : var_env -> frac -> place -> var_env) (gamma : var_env) (tau : ty) : var_env =
+  match tau with
+  | Ref (rgns, _, inner_tau) ->
+      let work (gamma : var_env) (rgn : rgn_atom) : var_env =
+        match rgn with
+        | RgnVar _ -> gamma
+        | Loan (_, frac, from_pi) -> change gamma frac from_pi
+      in walk change (List.fold_left work gamma rgns) inner_tau
+  | Array (inner_tau, _) -> walk change gamma inner_tau
+  | Tup taus -> List.fold_left (walk change) gamma taus
+  | _ -> gamma
 
 let incl (gamma : var_env) (tau : ty) : var_env =
-  let rec loop (gam : var_env) (tau : ty) : var_env =
-    match tau with
-    | Ref (rgns, _, inner_tau) ->
-        let work (gam : var_env) (rgn : rgn_atom) : var_env =
-          match rgn with
-          | RgnVar _ -> gam
-          | Loan (_, frac, from_pi) -> make_loan gam (frac_to_muta frac) from_pi
-        in loop (List.fold_left work gam rgns) inner_tau
-    | Array (inner_tau, _) -> loop gam inner_tau
-    | Tup taus -> List.fold_left loop gam taus
-    | _ -> gam
-  in loop gamma tau
+  walk (fun gamma -> fun frac -> make_loan gamma (frac_to_muta frac)) gamma tau
 
 let excl (gamma : var_env) (pi : place) : var_env =
-  let rec loop (gam : var_env) (tau : ty) : var_env =
-    match tau with
-    | Ref (rgns, _, inner_tau) ->
-        let work (gam : var_env) (rgn : rgn_atom) : var_env =
-          match rgn with
-          | RgnVar _ -> gam
-          | Loan (_, frac, from_pi) -> return_loan gam frac from_pi
-        in loop (List.fold_left work gam rgns) inner_tau
-    | Array (inner_tau, _) -> loop gam inner_tau
-    | Tup taus -> List.fold_left loop gam taus
-    | _ -> gam
-  in let (_, tau) = (var_env_lookup gamma pi)
-  in loop (List.remove_assoc pi gamma) tau
+  let (_, tau) = var_env_lookup gamma pi
+  in walk return_loan (List.remove_assoc pi gamma) tau
 
 let main =
   let env1 = [(Var 1, (Num 1, BaseTy U32))]
