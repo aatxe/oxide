d2b9f36ecf449a101cac0dd22bc690fc2f78e3ab
Aaron Weiss <awe@pdgn.co>
2019-06-04 14:24:38 -0400

    rest of t-branch

--- ocaml/typeck.ml
+++ ocaml/typeck.ml
@@ -8,6 +8,14 @@ let omega_safe (ell : loan_env) (gamma : place_env) (omega : owned) (pi : place_
 
 let unify (ell : loan_env) (ty1 : ty) (ty2 : ty) : loan_env * ty = failwith "unimplemented"
 
+let intersect (envs1 : loan_env * place_env) (envs2 : loan_env * place_env) : loan_env * place_env =
+  let (ell1, gamma1) = envs1
+  in let (ell2, gamma2) = envs2
+  in failwith "unimplemented"
+
+let valid_type (sigma : global_env) (delta : tyvar_env) (ell : loan_env) (gamma : place_env) (ty : ty) : () tc =
+  failwith "unimplemented"
+
 let type_check (sigma : global_env) (delta : tyvar_env) (ell : loan_env) (gamma : place_env)
                (expr : expr) : (ty * loan_env * place_env) tc =
   let rec tc (delta : tyvar_env) (ell : loan_env) (gamma : place_env)
@@ -29,7 +37,12 @@ let type_check (sigma : global_env) (delta : tyvar_env) (ell : loan_env) (gamma 
       (match tc delta ell gamma e1 with
        | Succ (BaseTy Bool, ell1, gamma1) ->
          (match (tc delta ell1 gamma1 e2, tc delta ell1 gamma1 e3) with
-          | (Succ (ty2, ell2, gamma2), Succ (ty3, ell3, gamma3)) -> failwith "unimplemented"
+          | (Succ (ty2, ell2, gamma2), Succ (ty3, ell3, gamma3)) ->
+            (let (ellPrime, gammaPrime) = intersect (ell2, gamma2) (ell3, gamm3)
+             in let (ellFinal, tyFinal) = unify ellPrime ty2 ty3
+             in match valid_type sigma delta ellFinal gammaPrime tyFinal with
+             | Succ () -> (tyFinal, ellFinal, gammaPrime)
+             | Fail err -> Fail err)
           | (Fail err, _) | (_, Fail err) -> Fail err)
        | Succ (found, _, _) -> Fail (TypeMismatch (fst e1, BaseTy Bool, found))
        | Fail err -> Fail err)
