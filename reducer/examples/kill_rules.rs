b8fb7f3473078041c960b9f398f5152a1d354da1
Aaron Weiss <awe@pdgn.co>
2019-06-26 17:51:37 -0400

    turned one of Niko's pseudocode examples into a real example

---
+++ reducer/examples/kill_rules.rs
@@ -0,0 +1,14 @@
+// this particular example is adapted from pseudocode from Niko
+fn drop<T>(x : T) {}
+
+fn main() {
+    let m: u32 = 0;
+    let q: &'q mut u32 = &mut m;
+    let r: &'r mut u32 = &mut *q; // loan L0 of `*q`
+    /* can't use `q` -- L0 in scope (because `r` is live) */
+    let n: u32 = 0;
+    q = &mut n; // kill(L0)
+    /* now I can use `q` */
+    drop::<&'q mut u32>(q);
+    drop::<&'r mut u32>(r);
+}
