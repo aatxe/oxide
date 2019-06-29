f1525b62ef6ebe7b0e22900cc951d0c4c348fc42
Aaron Weiss <awe@pdgn.co>
2019-06-28 21:46:54 -0400

    removed tests (to re-add as submodule)

--- tests/borrowck/regions-escape-unboxed-closure.rs
+++
@@ -1,8 +0,0 @@
-fn with_int(f: &mut dyn FnMut(&isize)) {
-}
-
-fn main() {
-    let mut x: Option<&isize> = None;
-    with_int(&mut |y| x = Some(y));
-    //~^ ERROR borrowed data cannot be stored outside of its closure
-}
--- tests/nll/issue-53040.rs
+++
@@ -1,7 +0,0 @@
-#![feature(nll)]
-
-fn main() {
-    let mut v: Vec<()> = Vec::new();
-    || &mut v;
-//~^ ERROR captured variable cannot escape `FnMut` closure body
-}
