6bebbe6c3b8edab1028df131fb6c12b5eced44f7
Aaron Weiss <awe@pdgn.co>
2019-06-28 21:51:12 -0400

    addded rustc test suite as a git submodule

---
+++ tests/nll/ty-outlives/ty-param-implied-bounds.rs
@@ -0,0 +1,32 @@
+// compile-flags:-Zborrowck=mir -Zverbose
+// compile-pass
+
+// Test that we assume that universal types like `T` outlive the
+// function body.
+
+#![allow(warnings)]
+#![feature(rustc_attrs)]
+
+use std::cell::Cell;
+
+fn twice<F, T>(value: T, mut f: F)
+where
+    F: FnMut(Cell<&T>),
+{
+    f(Cell::new(&value));
+    f(Cell::new(&value));
+}
+
+#[rustc_errors]
+fn generic<T>(value: T) {
+    // No error here:
+    twice(value, |r| invoke(r));
+}
+
+fn invoke<'a, T>(x: Cell<&'a T>)
+where
+    T: 'a,
+{
+}
+
+fn main() {}
