e8f64337b36bd2ce82aa4c9b3cbe35433863fd1c
weiss <weiss@ccs.neu.edu>
2019-07-09 10:58:19 -0400

    tweaked anonymous author email

--- anonymizer.sh
+++ anonymizer.sh
@@ -2,5 +2,5 @@
 
 git filter-branch --env-filter '
 export GIT_AUTHOR_NAME="Anonymous Possum"
-export GIT_AUTHOR_EMAIL="anon@example.com"
+export GIT_AUTHOR_EMAIL="git@localhost"
 ' --tag-name-filter cat -- --branches --tags
