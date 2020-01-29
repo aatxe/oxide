a7675b9d3bc1d0e395a14f0f535160428eb76bae
Aaron Weiss <awe@pdgn.co>
2020-01-29 11:30:04 -0500

    refactoring & cleanup in meta.ml

--- oxide/meta.ml
+++ oxide/meta.ml
@@ -35,9 +35,9 @@ let subst_prov (ty : ty) (this : prov) (that : prov) : ty =
     | Rec pairs -> (loc, Rec (sub_many_pairs pairs))
     | Tup tys -> (loc, Tup (sub_many tys))
     | Struct (name, provs, tys, tagged_ty) ->
-      let work (pv : prov) (provs : provs) =
+      let sub_next (pv : prov) (provs : provs) =
         List.cons (if (snd pv) = (snd that) then this else pv) provs
-      in let new_provs = List.fold_right work provs []
+      in let new_provs = List.fold_right sub_next provs []
       in (loc, Struct (name, new_provs, sub_many tys, sub_opt tagged_ty))
   and sub_many (tys : ty list) : ty list = List.map sub tys
   and sub_many_pairs (pairs : (field * ty) list) : (field * ty) list =
@@ -87,7 +87,8 @@ let rec decompose (ty : ty) (path : path) : (ty_ctx * ty) tc =
     in Succ (ctx, ty)
   | (Tup tys, (Index n) :: path) ->
     let* (inner_ctx, ty) = decompose (List.nth tys n) path
-    in let replace (idx : int) (ty : ty) : ty_ctx = if idx = n then inner_ctx else (fst ty, Ty ty)
+    in let replace (idx : int) (ty : ty) : ty_ctx =
+      if idx = n then inner_ctx else (fst ty, Ty ty)
     in let ctx : ty_ctx = (loc, Tup (List.mapi replace tys))
     in Succ (ctx, ty)
   | (Struct (_, _, _, Some ty), path) -> decompose ty path
@@ -131,11 +132,10 @@ let var_env_lookup (gamma : var_env) (pi : place) : ty tc =
     in Succ ty
   | None -> Fail (UnboundPlace pi)
 let var_env_lookup_many (gamma : var_env) (pis : place list) : ty list tc =
-  let lookup_seq (pi : place) (acc : ty list tc) : ty list tc =
-    let* tys = acc
-    in let* ty = var_env_lookup gamma pi
+  let lookup_seq (pi : place) (tys : ty list) : ty list tc =
+    let* ty = var_env_lookup gamma pi
     in Succ (List.cons ty tys)
-  in List.fold_right lookup_seq pis (Succ [])
+  in foldr lookup_seq pis []
 let var_env_lookup_opt (gamma : var_env) (pi : place) : (ty option) tc =
   let (root, path) = snd pi
   in match List.assoc_opt root gamma with
@@ -205,11 +205,10 @@ let eval_place_expr (ell : loan_env) (gamma : var_env) (omega : owned) (pi : pla
             let (_, (loc, (root, loan_path))) = loan
             in (loc, (root, List.append loan_path path))
           in let current_loans = List.map add_path_to_back loans
-          in let follow (pi : place_expr) (acc : loans tc) =
-            let* loans_so_far = acc
-            in let* new_loans = eval_place_expr pi
+          in let follow (pi : place_expr) (loans_so_far : loans) =
+            let* new_loans = eval_place_expr pi
             in Succ (List.append loans_so_far new_loans)
-          in List.fold_right follow current_loans (Succ [])
+          in foldr follow current_loans []
         | _ -> Fail (TypeMismatchRef ty))
     | _ :: _ -> failwith "unreachable"
   in eval_place_expr pi
@@ -222,11 +221,10 @@ let norm_place_expr (ell : loan_env) (gamma : var_env) (phi : place_expr) : plac
          List.map (fun pair -> snd pair) (List.filter (fun pair -> fst pair = None) progress)
     in let complete =
          List.map (fun pair -> unwrap (fst pair)) (List.filter (fun pair -> fst pair != None) progress)
-    in let continue (acc : places tc) (phi : place_expr) : places tc =
-         let* completed = acc
-         in let* newly_completed = norm phi
+    in let continue (completed : places) (phi : place_expr) : places tc =
+         let* newly_completed = norm phi
          in Succ (List.append completed newly_completed)
-    in List.fold_left continue (Succ complete) still_to_norm
+    in foldl continue complete still_to_norm
   in norm phi
 
 let rec is_prefix_of (path1 : expr_path) (path2 : expr_path) : bool =
@@ -247,26 +245,24 @@ let not_disjoint (pi : place_expr * place_expr) : bool =
 
 (* given a gamma, determines whether it is safe to use pi according to omega *)
 let is_safe (ell : loan_env) (gamma : var_env) (omega : owned) (phi : place_expr) : loans option tc =
-  let relevant (acc : loans tc) (loan : loan) : loans tc =
-    let (_, phi_prime) = loan
-    in let* acc = acc
-    in let* pis = norm_place_expr ell gamma phi_prime
+  let relevant (acc : loans) ((omega, phi_prime) : loan) : loans tc =
+    let* pis = norm_place_expr ell gamma phi_prime
     in if List.for_all not_disjoint (List.map (fun pi -> (phi, place_to_place_expr pi)) pis) then
-      Succ (List.cons loan acc)
+      Succ (List.cons (omega, phi_prime) acc)
     else Succ acc
   in match omega with
   | Unique -> (* for unique use to be safe, we need _no_ relevant loans *)
-    let* res = List.fold_left relevant (Succ []) (find_loans Shared ell phi)
+    let* res = foldl relevant [] (find_loans Shared ell phi)
     in (match res with
         | [] -> Succ None
         | loans -> Succ (Some loans))
   | Shared -> (* for shared use, we only care that there are no relevant _unique_ loans *)
-    let* res = List.fold_left relevant (Succ []) (find_loans Unique ell phi)
+    let* res = foldl relevant [] (find_loans Unique ell phi)
     in (match res with
         | [] -> Succ None
         | loans -> Succ (Some loans))
 
-let rec contains_prov (gamma : var_env) (prov : prov) =
+let rec contains_prov (gamma : var_env) (prov : prov) : bool =
   let rec ty_contains (ty : ty) : bool =
     match snd ty with
     | Any | BaseTy _ | TyVar _ -> false
@@ -284,23 +280,25 @@ let rec contains_prov (gamma : var_env) (prov : prov) =
   in List.exists (fun pair -> ty_contains (snd pair)) gamma
 
 let envs_minus (ell : loan_env) (gamma : var_env) (pi : place) : (loan_env * var_env) tc =
-  let rec loop (ty : ty option) (pair : (loan_env * var_env) tc) : (loan_env * var_env) tc =
+  let rec loop (ty : ty option) (envs : loan_env * var_env) : (loan_env * var_env) tc =
     match ty with
     | Some (_, Ref (prov, _, ty)) ->
-      let* (ell, gamma) = loop (Some ty) pair
+      let* (ell, gamma) = loop (Some ty) envs
       in let new_gamma = var_env_exclude gamma (sndfst pi)
       in if not (contains_prov new_gamma prov) then Succ (loan_env_exclude ell prov, new_gamma)
       else Succ (ell, new_gamma)
     | Some (_, Any) | Some (_, BaseTy _) | Some (_, TyVar _) | Some (_, Fun _)
-    | Some (_, Struct _) -> pair
-    | Some (_, Uninit ty) | Some (_, Array (ty, _)) | Some (_, Slice ty) -> loop (Some ty) pair
-    | Some (_, Rec pairs) -> loops (List.map snd pairs) pair
-    | Some (_, Tup tys) -> loops tys pair
-    | None -> Succ (ell, gamma)
-  and loops (tys : ty list) (pair : (loan_env * var_env) tc) =
-    List.fold_right loop (List.map (fun x -> Some x) tys) pair
+    | Some (_, Struct _) -> Succ envs
+    | Some (_, Uninit ty)
+    | Some (_, Array (ty, _))
+    | Some (_, Slice ty) -> loop (Some ty) envs
+    | Some (_, Rec pairs) -> loops (List.map snd pairs) envs
+    | Some (_, Tup tys) -> loops tys envs
+    | None -> Succ envs
+  and loops (tys : ty list) (envs : loan_env * var_env) =
+    foldr loop (List.map (fun x -> Some x) tys) envs
   in let* opt = var_env_lookup_opt gamma pi
-  in loop opt (Succ (ell, gamma))
+  in loop opt (ell, gamma)
 
 let rec path_prefixed_by (target : path) (in_path : path) : bool =
   match (target, in_path) with
@@ -347,26 +345,24 @@ let copyable (sigma : global_env) (typ : ty) : bool tc =
 let valid_copy_impl (sigma : global_env) (def : struct_def) : unit tc =
   match def with
   | Rec (true, name, _, _, fields) ->
-    let work (acc : (ty option) tc) (typ : ty) : (ty option) tc =
-      let* res = acc
-      in let* ty_copyable = copyable sigma typ
+    let next_copyable (res : ty option) (typ : ty) : (ty option) tc =
+      let* ty_copyable = copyable sigma typ
       in if (res == None) then
         if ty_copyable then Succ None
-        else acc
+        else Succ res
       else Succ (Some typ)
-    in (match List.fold_left work (Succ None) (List.map snd fields) with
+    in (match foldl next_copyable None (List.map snd fields) with
     | Succ None -> Succ ()
     | Succ (Some ty) -> Fail (InvalidCopyImpl (name, ty))
     | Fail err -> Fail err)
   | Tup (true, name, _, _, tys) ->
-    let work (acc : (ty option) tc) (typ : ty) : (ty option) tc =
-      let* res = acc
-      in let* ty_copyable = copyable sigma typ
+    let next_copyable (res : ty option) (typ : ty) : (ty option) tc =
+      let* ty_copyable = copyable sigma typ
       in if (res == None) then
         if ty_copyable then Succ None
-        else acc
+        else Succ res
       else Succ (Some typ)
-    in (match List.fold_left work (Succ None) tys with
+    in (match foldl next_copyable None tys with
     | Succ None -> Succ ()
     | Succ (Some ty) -> Fail (InvalidCopyImpl (name, ty))
     | Fail err -> Fail err)
@@ -422,8 +418,8 @@ and free_provs (expr : expr) : provs =
     List.flatten (provs :: List.map (fun x -> free_provs (snd x)) es)
   | TupStruct (_, provs, _, es) -> List.flatten (provs :: List.map free_provs es)
 
-let free_vars_helper (expr : expr) (should_include : var -> bool tc) : var list tc =
-   let rec free (expr : expr) : var list tc =
+let free_vars_helper (expr : expr) (should_include : var -> bool tc) : vars tc =
+   let rec free (expr : expr) : vars tc =
      match snd expr with
      | Prim _ | Fn _ | Abort _ -> Succ []
      | BinOp (_, e1, e2)
@@ -465,15 +461,14 @@ let free_vars_helper (expr : expr) (should_include : var -> bool tc) : var list 
        in Succ (List.concat [free1; free2; free3])
      | Tup exprs | Array exprs -> free_many exprs
      | RecStruct _ | TupStruct _ -> Succ [] (* FIXME: implement *)
-   and free_many (exprs : expr list) : var list tc =
-     let work (expr : expr) (acc : var list tc) : var list tc =
-       let* free_so_far = acc
-       in let* free = free expr
-       in Succ (List.append free free_so_far)
-     in List.fold_right work exprs (Succ [])
+   and free_many (exprs : expr list) : vars tc =
+     let next_free (expr : expr) (free_vars : var list) : vars tc =
+       let* free = free expr
+       in Succ (List.append free free_vars)
+     in foldr next_free exprs []
    in free expr
 
-let free_nc_vars (sigma : global_env) (gamma : var_env) (expr : expr) =
+let free_nc_vars (sigma : global_env) (gamma : var_env) (expr : expr) : vars tc =
   free_vars_helper expr
   (fun var ->
      match List.assoc_opt var gamma with
@@ -481,7 +476,7 @@ let free_nc_vars (sigma : global_env) (gamma : var_env) (expr : expr) =
      (* if it's not in gamma, that means it's local to the expression, thus isn't free *)
      | None -> Succ false)
 
-let free_vars (expr : expr) = free_vars_helper expr (fun _ -> Succ true)
+let free_vars (expr : expr) : vars tc = free_vars_helper expr (fun _ -> Succ true)
 
 let free_provs_var_env (gamma : var_env) : provs =
   List.flatten (List.map (fun x -> free_provs_ty (snd x)) gamma)
