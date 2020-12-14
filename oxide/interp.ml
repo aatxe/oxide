9f9a94f7113432ee087164e96a28dfb0eda88daa
Aaron Weiss <aweiss@hey.com>
2020-12-14 16:04:55 -0500

    global functions in interpreter

--- oxide/interp.ml
+++ oxide/interp.ml
@@ -7,6 +7,7 @@ type rt_error =
   | StuckAtValue of value
   | CannotMovePlaceExpr of place_expr
   | NotAnArray of value
+  | NoFunctionDefined of fn_var * global_env
 and 'a rt =
   | Succ of 'a
   | Fail of rt_error
@@ -14,11 +15,6 @@ and 'a rt =
 
 let succ (elem : 'a) : 'a rt = Succ elem
 let fail (err : rt_error) : 'a rt = Fail err
-
-let unwrap (rt : 'a rt) : 'a =
-  match rt with
-  | Succ x -> x
-  | Fail err ->
 
 let bind (rt : 'a rt) (f : 'a -> 'b rt) : 'b rt =
   match rt with
@@ -83,166 +79,183 @@ let value_ctx_in (sigma : store) (phi : place_expr) : value_ctx rt =
 let plug_value_ctx (ctx : value_ctx) (v : value) : value rt =
   failwith "unimplemented"
 
-let rec step (sigma : store) (e : expr) : (store * expr) rt =
-  match snd e with
-  | Prim prim -> StuckAtValue (PrimVal prim) |> fail
-  | BinOp (op, (_, Prim p1), (_, Prim p2)) ->
-    let* res = delta op p1 p2
-    in let ePrime : expr = (inferred, Prim res)
-    in (sigma, ePrime) |> succ
-  | BinOp (op, (loc, Prim p1), e2) ->
-    let* (sigmaPrime, e2Prime) = step sigma e2
-    in (sigmaPrime, (fst e, BinOp (op, (loc, Prim p1), e2Prime))) |> succ
-  | BinOp (op, e1, e2) ->
-    let* (sigmaPrime, e1Prime) = step sigma e1
-    in (sigmaPrime, (fst e, BinOp (op, e1Prime, e2))) |> succ
-  | Move phi ->
-    let* (_, value) = eval_place_expr sigma phi
-    in if copyable value then (sigma, value_to_expr value) |> succ
-    else (match place_expr_to_place phi with
-    | Some pi -> (moved pi sigma, value_to_expr value) |> succ
-    | None -> CannotMovePlaceExpr phi |> fail)
-  | Borrow (_, _, phi) ->
-    let* (referent, _) = eval_place_expr sigma phi
-    in let ptr : expr = (inferred, Ptr referent)
-    in (sigma, ptr) |> succ
-  | BorrowIdx (_, _, phi, (_, Prim (Num n))) ->
-    let* (referent, value) = eval_place_expr sigma phi
-    in (match value with
-    | ArrayVal values ->
-      if n < 0 then Aborted "negative array index out of bounds" |> fail
-      else if n > List.length values then Aborted "array index out of bounds" |> fail
-      else let ptr : expr = (inferred, Ptr referent)
+let step (globals : global_env) (sigma : store) (e : expr) : (store * expr) rt =
+  let rec step (sigma : store) (e : expr) : (store * expr) rt =
+    match snd e with
+    | Prim prim -> StuckAtValue (PrimVal prim) |> fail
+    | BinOp (op, (_, Prim p1), (_, Prim p2)) ->
+      let* res = delta op p1 p2
+      in let ePrime : expr = (inferred, Prim res)
+      in (sigma, ePrime) |> succ
+    | BinOp (op, (loc, Prim p1), e2) ->
+      let* (sigmaPrime, e2Prime) = step sigma e2
+      in (sigmaPrime, (fst e, BinOp (op, (loc, Prim p1), e2Prime))) |> succ
+    | BinOp (op, e1, e2) ->
+      let* (sigmaPrime, e1Prime) = step sigma e1
+      in (sigmaPrime, (fst e, BinOp (op, e1Prime, e2))) |> succ
+    | Move phi ->
+      let* (_, value) = eval_place_expr sigma phi
+      in if copyable value then (sigma, value_to_expr value) |> succ
+      else (match place_expr_to_place phi with
+      | Some pi -> (moved pi sigma, value_to_expr value) |> succ
+      | None -> CannotMovePlaceExpr phi |> fail)
+    | Borrow (_, _, phi) ->
+      let* (referent, _) = eval_place_expr sigma phi
+      in let ptr : expr = (inferred, Ptr referent)
       in (sigma, ptr) |> succ
-    | _ -> NotAnArray value |> fail)
-  | BorrowIdx (prov, omega, phi, idx) ->
-    let* (sigmaPrime, idxPrime) = step sigma idx
-    in (sigmaPrime, (fst e, BorrowIdx (prov, omega, phi, idxPrime))) |> succ
-  | BorrowSlice (_, _, phi, (_, Prim (Num n1)), (_, Prim (Num n2))) ->
-    let* (referent, value) = eval_place_expr sigma phi
-    in (match value with
-    | ArrayVal values ->
-      if n1 < 0 then Aborted "negative slice out of bounds" |> fail
-      else if n2 > List.length values then Aborted "slice out of bounds" |> fail
-      else if n1 > n2 then Aborted "negative-sized slice" |> fail
-      else let ptr : expr = (inferred, Ptr referent)
-      in (sigma, ptr) |> succ
-    | _ -> NotAnArray value |> fail)
-  | BorrowSlice (prov, omega, phi, ((_, Prim (Num _)) as idx1), idx2) ->
-    let* (sigmaPrime, idx2Prime) = step sigma idx2
-    in (sigmaPrime, (fst e, BorrowSlice (prov, omega, phi, idx1, idx2Prime))) |> succ
-  | BorrowSlice (prov, omega, phi, idx1, idx2) ->
-    let* (sigmaPrime, idx1Prime) = step sigma idx1
-    in (sigmaPrime, (fst e, BorrowSlice (prov, omega, phi, idx1Prime, idx2))) |> succ
-  | LetProv (_, e) -> (sigma, e) |> succ
-  | Let (var, ty, e1, e2) ->
-    if is_value e1 then let* v1 = expr_to_value e1
-    in (extend sigma var v1, e2) |> succ
-    else let* (sigmaPrime, e1Prime) = step sigma e1
-    in (sigmaPrime, (fst e, Let (var, ty, e1Prime, e2))) |> succ
-  | Assign (phi, exp) ->
-    if is_value exp then let x = expr_root_of phi
-    in let* ctx = value_ctx_in sigma phi
-    in let* new_value = exp |> expr_to_value >>= plug_value_ctx ctx
-    in let new_exp : expr = (fst e, Prim Unit)
-    in (update sigma x new_value, new_exp) |> succ
-    else let* (sigmaPrime, ePrime) = step sigma exp
-    in (sigmaPrime, (fst e, Assign (phi, ePrime))) |> succ
-  | Seq (e1, e2) ->
-    if is_value e1 then (sigma, e2) |> succ
-    else let* (sigmaPrime, e1Prime) = step sigma e1
-    in (sigmaPrime, (fst e, Seq (e1Prime, e2))) |> succ
-  | Fn fn_var -> StuckAtValue (FnVal fn_var) |> fail
-  | Fun ([], [], params, ret_ty, body) ->
-    (match free_vars body with
-    | Fail _ -> failwith "unreachable: free_vars on closure body cannot error at runtime"
-    | Succ xfs ->
-      let* xncs = free_nc_vars_rt sigma body
-      in let captured_frame = sigma |> List.flatten |> List.filter ((flip List.mem $ xfs) >> fst)
-      in let sigmaPrime = update_all sigma xncs Dead
-      in let ePrime : expr = (inferred, Closure (captured_frame, params, ret_ty, body))
-      in (sigmaPrime, ePrime) |> succ)
-  | App ((_, Fn _), _, _, _, _) -> failwith "unimplemented: need global ctx"
-  | App ((_, Closure (frame, params, _, body)) as cls, [], [], [], args) ->
-    let (already_values, to_be_evaluated) = partition is_value args
-    in if is_empty to_be_evaluated then
-      let vars = List.map fst params
-      in let arg_values : value list = List.map unsafe_expr_to_value args
-      in let top_frame = List.concat [List.combine vars arg_values; frame]
-      in (top_frame :: sigma, body) |> succ
-    else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
-    in let new_args = List.concat [already_values; [hdPrime]; List.tl to_be_evaluated]
-    in let ePrime : expr = (fst e, App (cls, [], [], [], new_args))
-    in (sigmaPrime, ePrime) |> succ
-  | App (e, envs, provs, tys, args) ->
-    let* (sigmaPrime, ePrime) = step sigma e
-    in (sigmaPrime, (fst e, App (ePrime, envs, provs, tys, args))) |> succ
-  | Idx (phi, (_, Prim (Num idx))) ->
-    (match eval_place_expr sigma phi with
-     | Succ (_, ArrayVal values) ->
-         if idx < 0 then Aborted "negative array index out of bounds" |> fail
-         else if idx > List.length values then Aborted "array index out of bounds" |> fail
-         else (sigma, List.nth values idx |> value_to_expr) |> succ
-     | Succ (_, value) -> NotAnArray value |> fail
-     | Fail err -> Fail err)
-  | Idx (phi, idx) ->
-    let* (sigmaPrime, idxPrime) = step sigma idx
-    in (sigmaPrime, (fst e, Idx (phi, idxPrime))) |> succ
-  | Abort msg -> Aborted msg |> fail
-  | Branch ((_, Prim True), cons, _) -> (sigma, cons) |> succ
-  | Branch ((_, Prim False), _, alt) -> (sigma, alt) |> succ
-  | Branch (cond, cons, alt) ->
-    let* (sigmaPrime, condPrime) = step sigma cond
-    in (sigmaPrime, (fst e, Branch (condPrime, cons, alt))) |> succ
-  | While (e1, e2) ->
-    let cons = (inferred, Seq (e2, e))
-    in let alt : expr = (inferred, Prim Unit)
-    in (sigma, (inferred, Branch (e1, cons, alt))) |> succ
-  | Tup exprs ->
-    let (already_values, to_be_evaluated) = partition is_value exprs
-    in if is_empty to_be_evaluated then
-      let* v = expr_to_value e
-      in StuckAtValue v |> fail
-    else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
-    in let ePrime : expr = (fst e, Tup (List.concat [already_values; [hdPrime];
-                                                     List.tl to_be_evaluated]))
-    in (sigmaPrime, ePrime) |> succ
-  | Array exprs ->
-    let (already_values, to_be_evaluated) = partition is_value exprs
-    in if is_empty to_be_evaluated then
-      let* v = expr_to_value e
-      in StuckAtValue v |> fail
-    else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
-    in let ePrime : expr = (fst e, Tup (List.concat [already_values; [hdPrime];
-                                                     List.tl to_be_evaluated]))
-    in (sigmaPrime, ePrime) |> succ
-  | RecStruct (sn, provs, tys, field_pairs) ->
-    let (already_values, to_be_evaluated) = partition (is_value >> snd) field_pairs
-    in if is_empty to_be_evaluated then
-      let* v = expr_to_value e
-      in StuckAtValue v |> fail
-    else let (field, hd) = List.hd to_be_evaluated
-    in let* (sigmaPrime, hdPrime) = step sigma hd
-    in let new_field_pairs = List.concat [already_values; [(field, hdPrime)];
-                                          List.tl to_be_evaluated]
-    in let ePrime : expr = (fst e, RecStruct (sn, provs, tys, new_field_pairs))
-    in (sigmaPrime, ePrime) |> succ
-  | TupStruct (sn, provs, tys, exprs) ->
-    let (already_values, to_be_evaluated) = partition is_value exprs
-    in if is_empty to_be_evaluated then
-      let* v = expr_to_value e
-      in StuckAtValue v |> fail
-    else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
-    in let new_exprs = (List.concat [already_values; [hdPrime];
-                                     List.tl to_be_evaluated])
-    in let ePrime : expr = (fst e, TupStruct (sn, provs, tys, new_exprs))
-    in (sigmaPrime, ePrime) |> succ
-  | Ptr referent -> StuckAtValue (PtrVal referent) |> fail
-  | _ -> failwith "unimplemented"
+    | BorrowIdx (_, _, phi, (_, Prim (Num n))) ->
+      let* (referent, value) = eval_place_expr sigma phi
+      in (match value with
+      | ArrayVal values ->
+        if n < 0 then Aborted "negative array index out of bounds" |> fail
+        else if n > List.length values then Aborted "array index out of bounds" |> fail
+        else let ptr : expr = (inferred, Ptr referent)
+        in (sigma, ptr) |> succ
+      | _ -> NotAnArray value |> fail)
+    | BorrowIdx (prov, omega, phi, idx) ->
+      let* (sigmaPrime, idxPrime) = step sigma idx
+      in (sigmaPrime, (fst e, BorrowIdx (prov, omega, phi, idxPrime))) |> succ
+    | BorrowSlice (_, _, phi, (_, Prim (Num n1)), (_, Prim (Num n2))) ->
+      let* (referent, value) = eval_place_expr sigma phi
+      in (match value with
+      | ArrayVal values ->
+        if n1 < 0 then Aborted "negative slice out of bounds" |> fail
+        else if n2 > List.length values then Aborted "slice out of bounds" |> fail
+        else if n1 > n2 then Aborted "negative-sized slice" |> fail
+        else let ptr : expr = (inferred, Ptr referent)
+        in (sigma, ptr) |> succ
+      | _ -> NotAnArray value |> fail)
+    | BorrowSlice (prov, omega, phi, ((_, Prim (Num _)) as idx1), idx2) ->
+      let* (sigmaPrime, idx2Prime) = step sigma idx2
+      in (sigmaPrime, (fst e, BorrowSlice (prov, omega, phi, idx1, idx2Prime))) |> succ
+    | BorrowSlice (prov, omega, phi, idx1, idx2) ->
+      let* (sigmaPrime, idx1Prime) = step sigma idx1
+      in (sigmaPrime, (fst e, BorrowSlice (prov, omega, phi, idx1Prime, idx2))) |> succ
+    | LetProv (_, e) -> (sigma, e) |> succ
+    | Let (var, ty, e1, e2) ->
+      if is_value e1 then let* v1 = expr_to_value e1
+      in (extend sigma var v1, e2) |> succ
+      else let* (sigmaPrime, e1Prime) = step sigma e1
+      in (sigmaPrime, (fst e, Let (var, ty, e1Prime, e2))) |> succ
+    | Assign (phi, exp) ->
+      if is_value exp then let x = expr_root_of phi
+      in let* ctx = value_ctx_in sigma phi
+      in let* new_value = exp |> expr_to_value >>= plug_value_ctx ctx
+      in let new_exp : expr = (fst e, Prim Unit)
+      in (update sigma x new_value, new_exp) |> succ
+      else let* (sigmaPrime, ePrime) = step sigma exp
+      in (sigmaPrime, (fst e, Assign (phi, ePrime))) |> succ
+    | Seq (e1, e2) ->
+      if is_value e1 then (sigma, e2) |> succ
+      else let* (sigmaPrime, e1Prime) = step sigma e1
+      in (sigmaPrime, (fst e, Seq (e1Prime, e2))) |> succ
+    | Fn fn_var -> StuckAtValue (FnVal fn_var) |> fail
+    | Fun ([], [], params, ret_ty, body) ->
+      (match free_vars body with
+      | Fail _ -> failwith "unreachable: free_vars on closure body cannot error at runtime"
+      | Succ xfs ->
+        let* xncs = free_nc_vars_rt sigma body
+        in let captured_frame = sigma |> List.flatten |> List.filter ((flip List.mem $ xfs) >> fst)
+        in let sigmaPrime = update_all sigma xncs Dead
+        in let ePrime : expr = (inferred, Closure (captured_frame, params, ret_ty, body))
+        in (sigmaPrime, ePrime) |> succ)
+    | App ((_, Fn fn_var) as fn, _, _, _, args) ->
+      let (already_values, to_be_evaluated) = partition is_value args
+      in if is_empty to_be_evaluated then
+        match global_env_find_fn globals fn_var with
+        | Some (_, _, _, _, params, _, _, body) ->
+          let vars = List.map fst params
+          in let arg_values : value list = List.map unsafe_expr_to_value args
+          in let top_frame = List.combine vars arg_values
+          in (top_frame :: sigma, body) |> succ
+          (* FIXME: do substitution, though I'm not sure it matters for runtime only *)
+        | None -> NoFunctionDefined (fn_var, globals) |> fail
+      else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
+      in let new_args = List.concat [already_values; [hdPrime]; List.tl to_be_evaluated]
+      in let ePrime : expr = (fst e, App (fn, [], [], [], new_args))
+      in (sigmaPrime, ePrime) |> succ
+    | App ((_, Closure (frame, params, _, body)) as cls, [], [], [], args) ->
+      let (already_values, to_be_evaluated) = partition is_value args
+      in if is_empty to_be_evaluated then
+        let vars = List.map fst params
+        in let arg_values : value list = List.map unsafe_expr_to_value args
+        in let top_frame = List.concat [List.combine vars arg_values; frame]
+        in (top_frame :: sigma, body) |> succ
+      else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
+      in let new_args = List.concat [already_values; [hdPrime]; List.tl to_be_evaluated]
+      in let ePrime : expr = (fst e, App (cls, [], [], [], new_args))
+      in (sigmaPrime, ePrime) |> succ
+    | App (e, envs, provs, tys, args) ->
+      let* (sigmaPrime, ePrime) = step sigma e
+      in (sigmaPrime, (fst e, App (ePrime, envs, provs, tys, args))) |> succ
+    | Idx (phi, (_, Prim (Num idx))) ->
+      (match eval_place_expr sigma phi with
+      | Succ (_, ArrayVal values) ->
+          if idx < 0 then Aborted "negative array index out of bounds" |> fail
+          else if idx > List.length values then Aborted "array index out of bounds" |> fail
+          else (sigma, List.nth values idx |> value_to_expr) |> succ
+      | Succ (_, value) -> NotAnArray value |> fail
+      | Fail err -> Fail err)
+    | Idx (phi, idx) ->
+      let* (sigmaPrime, idxPrime) = step sigma idx
+      in (sigmaPrime, (fst e, Idx (phi, idxPrime))) |> succ
+    | Abort msg -> Aborted msg |> fail
+    | Branch ((_, Prim True), cons, _) -> (sigma, cons) |> succ
+    | Branch ((_, Prim False), _, alt) -> (sigma, alt) |> succ
+    | Branch (cond, cons, alt) ->
+      let* (sigmaPrime, condPrime) = step sigma cond
+      in (sigmaPrime, (fst e, Branch (condPrime, cons, alt))) |> succ
+    | While (e1, e2) ->
+      let cons = (inferred, Seq (e2, e))
+      in let alt : expr = (inferred, Prim Unit)
+      in (sigma, (inferred, Branch (e1, cons, alt))) |> succ
+    | Tup exprs ->
+      let (already_values, to_be_evaluated) = partition is_value exprs
+      in if is_empty to_be_evaluated then
+        let* v = expr_to_value e
+        in StuckAtValue v |> fail
+      else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
+      in let ePrime : expr = (fst e, Tup (List.concat [already_values; [hdPrime];
+                                                      List.tl to_be_evaluated]))
+      in (sigmaPrime, ePrime) |> succ
+    | Array exprs ->
+      let (already_values, to_be_evaluated) = partition is_value exprs
+      in if is_empty to_be_evaluated then
+        let* v = expr_to_value e
+        in StuckAtValue v |> fail
+      else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
+      in let ePrime : expr = (fst e, Tup (List.concat [already_values; [hdPrime];
+                                                      List.tl to_be_evaluated]))
+      in (sigmaPrime, ePrime) |> succ
+    | RecStruct (sn, provs, tys, field_pairs) ->
+      let (already_values, to_be_evaluated) = partition (is_value >> snd) field_pairs
+      in if is_empty to_be_evaluated then
+        let* v = expr_to_value e
+        in StuckAtValue v |> fail
+      else let (field, hd) = List.hd to_be_evaluated
+      in let* (sigmaPrime, hdPrime) = step sigma hd
+      in let new_field_pairs = List.concat [already_values; [(field, hdPrime)];
+                                            List.tl to_be_evaluated]
+      in let ePrime : expr = (fst e, RecStruct (sn, provs, tys, new_field_pairs))
+      in (sigmaPrime, ePrime) |> succ
+    | TupStruct (sn, provs, tys, exprs) ->
+      let (already_values, to_be_evaluated) = partition is_value exprs
+      in if is_empty to_be_evaluated then
+        let* v = expr_to_value e
+        in StuckAtValue v |> fail
+      else let* (sigmaPrime, hdPrime) = to_be_evaluated |> List.hd |> step sigma
+      in let new_exprs = (List.concat [already_values; [hdPrime];
+                                      List.tl to_be_evaluated])
+      in let ePrime : expr = (fst e, TupStruct (sn, provs, tys, new_exprs))
+      in (sigmaPrime, ePrime) |> succ
+    | Ptr referent -> StuckAtValue (PtrVal referent) |> fail
+    | Closure (frame, params, ret_ty, body) ->
+      StuckAtValue (ClosureVal (frame, params, ret_ty, body)) |> fail
+  in step sigma e
 
-let interp (exp : expr) : value rt =
+let interp (globals : global_env) (exp : expr) : value rt =
   let rec loop (sigma : store) (exp : expr) : value rt =
     if is_value exp then expr_to_value exp
-    else let* (sigmaPrime, expPrime) = step sigma exp
+    else let* (sigmaPrime, expPrime) = step globals sigma exp
     in loop sigmaPrime expPrime
   in loop [] exp
